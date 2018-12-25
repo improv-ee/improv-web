01caf2df982dc4a7332dbaa84a83bae40ad30f35
Ando Roots <ando@sqroot.eu>
2018-12-25 14:51:18 +0200

    Require min length on profile creation

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -50,8 +50,8 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|string|max:255',
-            'username' => 'required|string|max:32|unique:users',
-            'email' => 'required|string|email|max:255|unique:users',
+            'username' => 'required|string|min:5|max:32|unique:users',
+            'email' => 'required|string|email|min:6|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed|pwned',
         ]);
     }
