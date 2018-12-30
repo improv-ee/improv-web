e7d76a2a3636f7b049989370845cbb4a7c68ca25
Ando Roots <ando@sqroot.eu>
2018-12-30 18:43:30 +0200

    Add old logo to header

--- src/resources/views/layouts/_header.blade.php
+++ src/resources/views/layouts/_header.blade.php
@@ -6,10 +6,14 @@
 
 <header class="blog-header py-3">
     <div class="row align-items-center">
-        <div class="col-12 col-md-8 text-center">
-            <h1><a href="/">{{ config('app.name') }}</a></h1>
+        <div class="col-12 col-md-8">
+            <h1>
+                <a href="/">
+                    <img src="{{ asset('img/logo.png') }}" height="50" alt="Logo" />    {{ config('app.name') }}
+                </a>
+            </h1>
         </div>
-        <div class="col-12 col-md-4 align-items-center">
+        <div class="col-12 col-md-4 align-items-end text-right">
             @yield('topright')
         </div>
     </div>
