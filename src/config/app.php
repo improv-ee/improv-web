b7a4fde48ea9e21f7cfa87c929bd75119c291c56
Ando Roots <ando@sqroot.eu>
2019-01-06 16:33:22 +0200

    Set reasonable defaults to env variables

--- src/config/app.php
+++ src/config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Improv Estonia'),
+    'name' => env('APP_NAME', 'Scenepartner'),
 
     /*
     |--------------------------------------------------------------------------
