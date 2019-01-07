eeb06b879bf06eac455ed315989799e5a2b19668
Ando Roots <ando@sqroot.eu>
2019-01-07 23:23:39 +0200

    Add debugging statements

--- .travis/build-webserver.sh
+++ .travis/build-webserver.sh
@@ -8,10 +8,16 @@ cd src
 
 touch .env
 
+echo "Installing Composer dependencies"
 composer install --no-interaction --prefer-dist --no-dev
 
+echo "Installing NPM dependencies"
 npm install
+
+echo "Generating static assets"
 npm run prod
+
+cd ..
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH | tr / - ; fi`
