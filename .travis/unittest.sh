b1241bd2fa52c12e35ad40d6696146414e50de6b
Ando Roots <ando@sqroot.eu>
2019-01-07 21:21:34 +0200

    Use Travis cache

--- .travis/unittest.sh
+++ .travis/unittest.sh
@@ -5,9 +5,8 @@ set -e
 cd src
 touch .env
 
+echo "Installing Composer dependencies..."
 composer install --no-interaction
-npm install
 
-php artisan passport:install
-
+echo "Running unit tests..."
 php vendor/bin/phpunit
