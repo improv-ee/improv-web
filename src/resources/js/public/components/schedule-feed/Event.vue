ac16b2169f0a4d0c7798878a3a3db831ea9fe07f
Ando Roots <ando@sqroot.eu>
2019-02-02 21:47:20 +0200

    Fix priorities of title and excerpt rendering on ScheduleFeed

--- src/resources/js/public/components/schedule-feed/Event.vue
+++ src/resources/js/public/components/schedule-feed/Event.vue
@@ -2,12 +2,12 @@
 
     <div class="card h-100 mb-4 shadow-sm event-card">
         <div class="view overlay">
-        <router-link :to="{ name: 'events', params: { uid: event.uid }}">
+            <router-link :to="{ name: 'events', params: { uid: event.uid }}">
                 <img class="card-img-top flex-auto"
                      :src="header_img(event.production.images)"
-                     :alt="event.production.slug" />
-            <div class="mask rgba-white-slight"></div>
-        </router-link>
+                     :alt="event.production.slug"/>
+                <div class="mask rgba-white-slight"></div>
+            </router-link>
         </div>
 
         <!-- Button -->
@@ -15,11 +15,11 @@
 
         <div class="card-body align-items-start">
 
-                <router-link :to="{ name: 'events', params: { uid: event.uid }}"><h4 class="mb-0">
-                    {{ event.production.title }}
-                </h4></router-link>
-                <p class="card-text mb-auto">{{ event.production.excerpt }}</p>
-            </div>
+            <router-link :to="{ name: 'events', params: { uid: event.uid }}"><h4 class="mb-0">
+                {{ title }}
+            </h4></router-link>
+            <p class="card-text mb-auto">{{ excerpt }}</p>
+        </div>
 
 
         <div class="rounded-bottom deep-purple pt-3">
@@ -40,9 +40,26 @@
         computed: {
             start_time: function () {
                 return moment(this.event.times.start).format('Do MMMM HH:mm')
-            }
-
             },
+            title: function(){
+                if (this.event.title){
+                    return this.event.title;
+                }
+                return this.event.production.title;
+            },
+            excerpt: function () {
+                if (this.event.production.excerpt) {
+                    return this.event.production.excerpt;
+                }
+
+                if (this.event.description) {
+                    return _.truncate(this.event.description, {length: 200})
+                }
+                if (this.event.production.description) {
+                    return _.truncate(this.event.production.description, {length: 200})
+                }
+            }
+        },
         methods: {
             header_img(images) {
                 return images.header ? images.header.urls.original : '/img/production/default-header.jpg';
