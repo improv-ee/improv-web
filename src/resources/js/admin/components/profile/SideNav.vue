c57b3f16d564030b714a2856822f5d27e8779c7b
Ando Roots <ando@sqroot.eu>
2019-02-24 17:08:50 +0200

    Add missin SideNav component

---
+++ src/resources/js/admin/components/profile/SideNav.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="col-2">
+        <b-nav vertical class="side-nav">
+            <b-nav-item :to="{ name: 'profile.settings' }">{{ $t("user.profile.settings") }}</b-nav-item>
+            <b-nav-item :to="{ name: 'profile.api.tokens' }">{{ $t("user.profile.api_tokens") }}</b-nav-item>
+            <b-nav-item :to="{ name: 'profile.invite' }">{{ $t("user.profile.invite.send_invitation") }}</b-nav-item>
+        </b-nav>
+    </div>
+</template>
