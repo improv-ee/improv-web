afcbc4204ebd7c8738850343cdf61d963f7c1e9a
Ando Roots <ando@sqroot.eu>
2019-05-19 17:07:31 +0300

    Add missing English translations
    Ref #7

--- src/resources/lang/en/auth.php
+++ src/resources/lang/en/auth.php
@@ -17,5 +17,14 @@ return [
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
     'login' => 'Login',
     'register' => 'Sign up',
-    'logout' => 'Log out'
+    'logout' => 'Log out',
+    'forgot_password' => 'Forgot password?',
+    'send_reset_link' => 'Send forgotten password link',
+    'reset_password' => 'Reset password',
+    'remember_me' => 'Remember me',
+    'verify_email' => 'Confirm your e-mail address',
+    'email_verification_needed' => 'We sent an e-mail verification link to your e-mail. Please open the message and click on the enclosed verification link.',
+    'email_verification_did_not_receive' => ' If the e-mail did not reach you',
+    'email_verification_request_another' => 'click here to send another',
+    'email_verification_resent' => 'New verification e-mail sent'
 ];
---
+++ src/resources/lang/en/site.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'meta_description' => 'improv.ee is a website for improvised theatre schedule and info',
+    'meta_keywords' => 'improteater,impro,improv,improv in estonia,improvisation,improv show',
+    'more_info' => 'Read more',
+];
