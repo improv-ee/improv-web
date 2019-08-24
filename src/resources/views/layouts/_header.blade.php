de4085e356f87de50c4351f65e62d93c551017cf
Ando Roots <ando@sqroot.eu>
2019-08-24 17:24:18 +0300

    Add Impro10 logo

--- src/resources/views/layouts/_header.blade.php
+++ src/resources/views/layouts/_header.blade.php
@@ -1,6 +1,7 @@
 <header class="site-header py-3">
     <div class="row align-items-center">
         <div class="col-12 col-md-8">
+            <img src="{{ asset('img/impro10.png') }}" alt="Impro 10" class="impro-10" />
             <h1>
                 <a href="/">
                     <img src="{{ asset('img/logo.png') }}" height="50" alt="Logo" />    {{ config('app.name') }}
