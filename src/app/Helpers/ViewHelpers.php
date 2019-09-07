improv-ee/improv-web

commit 26becf628477b848528acdd23c2070590456dd1d

Embed app config into HTML, do noy load via XHR
When the JS frontend app is started, it needs some config
about current app state and logged in user.

This was loaded via an API endpoint, but it causes a blocking
delay for one round-trip.

In order to avoid this and speed up the app, let's embed
the initial config to the HTML, loaded during the first
request.

src/app/Helpers/ViewHelpers.php
@@ -1,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
+
+if (!function_exists('appConfigJson')) {
+
+    /**
+     * Get app config in JSON
+     *
+     * This is used in the main view, to echo some intial JSON
+     * config for the JS frontend to load.
+     *
+     * @return string Application main config in JSON form
+     */
+    function appConfigJson()
+    {
+        return json_encode([
+            'token' => Request::instance()->session()->get('apiToken'),
+            'apiUrl' => sprintf('https://%s/v1', env('API_DOMAIN')),
+            'username' => Auth::user()->username ?? null,
+            'release' => [
+                'version' => getenv('RELEASE_VERSION', 'dev'),
+                'timestamp' => getenv('RELEASE_TIME', null)
+            ],
+            'languages' => [
+                'supported' => config('language.allowed'),
+                'current' => Request::instance()->getLocale()
+            ]], JSON_UNESCAPED_UNICODE);
+    }
+}

src/resources/js/admin/app.js
@@ -103,29 +103,17 @@ function setI18nLanguage (lang) {
     return lang;
 }
 
+// eslint-disable-next-line no-console
+console.log('Initializing app...');
 
-function bootApp() {
+// Set language
+setI18nLanguage(window.config.languages.current);
 
-    // Wait for async Axios request to load app config...
-    if (!window.config) {
-        setTimeout(bootApp, 50);
-        return;
-    }
-    // eslint-disable-next-line no-console
-    console.log('Initializing app...');
+window.axios.defaults.headers.common['Authorization'] = 'Bearer ' + window.config.token;
 
-    // Set language
-    setI18nLanguage(window.config.languages.current);
-
-    window.axios.defaults.headers.common['Authorization'] = 'Bearer ' + window.config.token;
-
-    new Vue({
-        el: '#app',
-        render: createElement => createElement(App),
-        router,
-        i18n
-    });
-}
-
-bootApp();
-
+new Vue({
+    el: '#app',
+    render: createElement => createElement(App),
+    router,
+    i18n
+});

src/resources/js/bootstrap.js
@@ -69,12 +69,6 @@ if (token) {
 }
 
 console.log('Initializing app, requesting runtime config from backend...');
-axios.get('/getConfig')
-    .then(response => {
-        window.config = response.data;
-        console.info('Config loaded, API URL is ' + window.config.apiUrl + ', continuing with app init...');
-    }).catch(function (error) {
-        console.error('Fatal - unable to load app config!');
-        console.error(error);
-        window.location.href = '/maintenance';
-    });
+const jsonNode = document.getElementById('app-config');
+window.config = JSON.parse(jsonNode.textContent);
+console.info('Config loaded, API URL is ' + window.config.apiUrl + ', continuing with app init...');

src/resources/js/public/app.js
@@ -77,26 +77,15 @@ function setI18nLanguage (lang) {
     return lang;
 }
 
+// eslint-disable-next-line no-console
+console.log('Initializing app...');
 
-function bootApp() {
+// Set language
+setI18nLanguage(window.config.languages.current);
 
-    // Wait for async Axios request to load app config...
-    if (!window.config) {
-        setTimeout(bootApp, 50);
-        return;
-    }
-    // eslint-disable-next-line no-console
-    console.log('Initializing app...');
-
-    // Set language
-    setI18nLanguage(window.config.languages.current);
-
-    new Vue({
-        el: '#app',
-        render: createElement => createElement(App),
-        router,
-        i18n
-    });
-}
-
-bootApp();
+new Vue({
+    el: '#app',
+    render: createElement => createElement(App),
+    router,
+    i18n
+});

src/app/Http/Controllers/HomeController.php
@@ -3,10 +3,6 @@
 namespace App\Http\Controllers;
 
 
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
 
@@ -18,33 +14,6 @@ class HomeController extends Controller
     public function index()
     {
         return view('frontpage');
-    }
-
-    /**
-     * Return application config to the frontend
-     *
-     * Logged in users also get an api token, which enables them to do authenticated requests
-     * to the backend API.
-     *
-     * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function getConfig(Request $request)
-    {
-        return response()
-            ->json([
-                'token' => $request->session()->get('apiToken'),
-                'apiUrl' => sprintf('https://%s/v1', env('API_DOMAIN')),
-                'username' => Auth::user()->username ?? null,
-                'release' => [
-                    'version' => getenv('RELEASE_VERSION', 'dev'),
-                    'timestamp' => getenv('RELEASE_TIME', null)
-                ],
-                'languages' => [
-                    'supported' => config('language.allowed'),
-                    'current' => app::getLocale()
-                ],
-            ])->withHeaders(['Cache-Control' => 'private']);
     }
 
     public function maintenance()

src/app/Providers/HelperServiceProvider.php
@@ -1,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelperServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->loadHelpers();
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    protected function loadHelpers()
+    {
+        foreach (glob(__DIR__.'/../Helpers/*.php') as $filename)
+        {
+            require_once $filename;
+        }
+    }
+}

src/config/app.php
@@ -166,7 +166,7 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\HelperServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 

src/resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
     @yield('content')
 
 </div>
+<script type="application/json" id="app-config">{!! appConfigJson() !!}</script>
 @yield('scripts')
 </body>
 </html>

commit 207ef03568878777037ea80089bb52e13b255766

Fix app language switching
This was broken in 26becf628477b848528acdd23c2070590456dd1d.
Use APP language, not Request language in global config.

src/app/Helpers/ViewHelpers.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Request;
 
@@ -25,7 +26,7 @@ if (!function_exists('appConfigJson')) {
             ],
             'languages' => [
                 'supported' => config('language.allowed'),
-                'current' => Request::instance()->getLocale()
+                'current' => App::getLocale()
             ]], JSON_UNESCAPED_UNICODE);
     }
 }

src/resources/js/admin/app.js
@@ -104,7 +104,7 @@ function setI18nLanguage (lang) {
 }
 
 // eslint-disable-next-line no-console
-console.log('Initializing app...');
+console.log('Initializing Vue...');
 
 // Set language
 setI18nLanguage(window.config.languages.current);

src/resources/js/bootstrap.js
@@ -68,7 +68,7 @@ if (token) {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
-console.log('Initializing app, requesting runtime config from backend...');
+console.log('Initializing app, loading runtime config...');
 const jsonNode = document.getElementById('app-config');
 window.config = JSON.parse(jsonNode.textContent);
 console.info('Config loaded, API URL is ' + window.config.apiUrl + ', continuing with app init...');