1347aa93f3c84586f707e98b2340733467354ae6
Ando Roots <ando@sqroot.eu>
2019-09-26 22:50:53 +0300

    Reformat Vue

--- src/resources/js/public/components/production/OtherEvents.vue
+++ src/resources/js/public/components/production/OtherEvents.vue
@@ -1,29 +1,31 @@
 <template>
-    <div class="row" v-if="production.loaded() && production.getEvents().length > 1">
-      <div class="col-md-12 col-lg-12">
-        <h1>{{ $t('production.events') }}</h1>
-        <router-link
-                v-for="event in production.getEvents()"
-                :key="event.uid"
-                class="btn btn-primary"
-                :to="{name: 'events', params: {uid: event.getUid()}}">
-          {{ event.getStartTimeHuman() }}
-        </router-link>
-      </div>
+  <div
+    v-if="production.loaded() && production.getEvents().length > 1"
+    class="row">
+    <div class="col-md-12 col-lg-12">
+      <h1>{{ $t('production.events') }}</h1>
+      <router-link
+        v-for="event in production.getEvents()"
+        :key="event.uid"
+        class="btn btn-primary"
+        :to="{name: 'events', params: {uid: event.getUid()}}">
+        {{ event.getStartTimeHuman() }}
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-    import {Production} from '../../../models/production';
-    export default {
+import {Production} from '../../../models/production';
+export default {
 
-        props: {
-            production: {
-                type: Object,
-                default: function () {
-                    return new Production();
-                }
+    props: {
+        production: {
+            type: Object,
+            default: function () {
+                return new Production();
             }
         }
-    };
+    }
+};
 </script>
